ad29b914e1328768b2eeddae75bda463f9e0fd6b
bea kirkbright <bzkirkbright@gmail.com>
2024-11-15 17:16:56 -0500

    added chromatic static site

--- designSystem/.storybook/GGTheme.js
+++ designSystem/.storybook/GGTheme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'light',
-  brandTitle: 'Hill`s Theme',
-  brandImage: 'https://www.hillspet.com/content/dam/cp-sites-aem/hillspet-brand/mlp-assets/home-page/hills-logo.png',
+  brandTitle: 'GG Theme',
+  brandImage: 'storybook.svg',
   brandTarget: '_self'
 });
